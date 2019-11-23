cb7705ee0d81f086025a0e970b1254f9d1f571b5
Ian Johnson <ianprime0509@gmail.com>
2019-11-23 07:21:50 -0500

    Add battery status to StumpWM mode line

--- stumpwm/.stumpwm.d/init.lisp
+++ stumpwm/.stumpwm.d/init.lisp
@@ -8,7 +8,7 @@
 
 ;;; Wallpaper
 
-(defparameter *my-wallpaper-path* "/home/ian/Pictures/bocchi.png")
+(defparameter *my-wallpaper-path* "/home/ian/Pictures/mio.png")
 (run-shell-command (format nil "feh --no-fehbg --bg-fill '~a'"
                            *my-wallpaper-path*))
 
@@ -38,36 +38,69 @@
 
 ;;; Mode line
 
-(setf *screen-mode-line-format* "[^B%n^b] %d | %W")
+(setf *screen-mode-line-format* "[^B%n^b] %d | %t | %W")
 (setf *time-modeline-string* "%a %b %e %k:%M")
-(setf *mode-line-timeout* 1)
+(setf *mode-line-timeout* 5)
 (enable-mode-line (current-screen) (current-head) t)
+(add-screen-mode-line-formatter #\t
+                                (lambda (&rest args)
+                                  (declare (ignore args))
+                                  (format nil "~a ~d%"
+                                          (get-battery-status "BAT0")
+                                          (round (get-battery-charge "BAT0")))))
 
 ;;; Commands
 
 (defcommand emacs () ()
-  "Start Emacs unless it is already running, in which case focus it."
-  (run-or-raise "emacsclient -c" '(:class "Emacs")))
+  "Start Emacs unless it is already running in the current group, in which case focus it."
+  (run-or-raise "emacsclient -c" '(:class "Emacs") nil))
 
 (defcommand raise-volume (amount)
     ((:number "Percent to increase volume: "))
-  (run-shell-command (format nil "pamixer -i ~d" amount))
+  (run-shell-command (format nil "pamixer -i ~d" amount) t)
   (message "Volume: ~d%" (get-volume)))
 
 (defcommand lower-volume (amount)
     ((:number "Percent to lower volume: "))
-  (run-shell-command (format nil "pamixer -d ~d" amount))
+  (run-shell-command (format nil "pamixer -d ~d" amount) t)
   (message "Volume: ~d%" (get-volume)))
 
 ;;; Keys
 
 (run-shell-command "setxkbmap -option caps:ctrl_modifier")
+(run-shell-command "ibus-daemon -drx")
 
 (define-key *top-map* (kbd "XF86AudioRaiseVolume") "raise-volume 5")
 (define-key *top-map* (kbd "XF86AudioLowerVolume") "lower-volume 5")
 
 ;;; Helper functions
 
+(defconstant +battery-directory+ "/sys/class/power_supply/")
+
+(defun get-battery-charge (battery)
+  "Return the remaining charge of BATTERY as a number between 0 and 100 (inclusive)."
+  (let* ((charge-path (concatenate 'string
+                                   +battery-directory+ battery "/charge_now"))
+         (charge-full-path (concatenate 'string
+                                        +battery-directory+ battery
+                                        "/charge_full"))
+         (charge (with-open-file (input charge-path)
+                   (parse-integer (read-line input))))
+         (charge-full (with-open-file (input charge-full-path)
+                        (parse-integer (read-line input)))))
+    (* 100 (/ charge charge-full))))
+
+(defun get-battery-status (battery)
+  "Return the current status (e.g. discharging) of BATTERY."
+  (let ((status-path (concatenate 'string
+                                  +battery-directory+ battery "/status")))
+    (with-open-file (input status-path)
+      (read-line input))))
+
 (defun get-volume ()
   "Return the current audio volume as a number between 0 and 100 (inclusive)."
   (parse-integer (run-shell-command "pamixer --get-volume" t)))
+
+(defun strip-string (string)
+  "Remove leading and trailing whitespace from STRING."
+  (string-trim #(#\Space #\Newline #\Tab) string))
